e02db59b5f1d9d6c21cab72cec036b04140b2234
prailmv <prailprakash@gmail.com>
2020-05-07 13:49:40 +0530

    added recipe details

--- recipe/tests/test_recipe_api.py
+++ recipe/tests/test_recipe_api.py
@@ -5,12 +5,21 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer,RecipeDetailSerializer
 
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+def detail_url(recipe_id):
+    return reverse('recipe:recipe-detail',args=[recipe_id])
+
+def sample_tag(user, name='Main Course'):
+    return Tag.objects.create(user=user,name=name)
+
+def sample_ingredient(user,name='Cinnamon'):
+    return Ingredient.objects.create(user=user, name=name)
 
 
 def sample_recipe(user, **params):
@@ -77,3 +86,65 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_details(self):
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_basic_recipe(self):
+        payload={
+            'title':'chocolate cheese',
+            'time_minutes':30,
+            'price':4.00
+        }
+
+        res = self.client.post(RECIPES_URL,payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        for key in payload.keys():
+            self.assertEqual(payload[key], getattr(recipe,key))
+
+    def test_create_recipe_with_tags(self):
+        tag1 = sample_tag(user=self.user, name = 'Vegan')
+        tag2 = sample_tag(user=self.user, name= 'Desset')
+        payload = {
+            'title':'avacado shake',
+            'tags':[tag1.id,tag2.id],
+            'time_minutes':30,
+            'price':6.00
+        }
+        res = self.client.post(RECIPES_URL,payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        tags = recipe.tags.all()
+        self.assertEqual(tags.count(),2)
+        self.assertIn(tag1,tags)
+        self.assertIn(tag2,tags)
+
+    def test_create_recipe_with_ingredients(self):
+        """Test creating recipe with ingredients"""
+        ingredient1 = sample_ingredient(user=self.user, name='Ingredient 1')
+        ingredient2 = sample_ingredient(user=self.user, name='Ingredient 2')
+        payload = {
+            'title': 'Test recipe with ingredients',
+            'ingredients': [ingredient1.id, ingredient2.id],
+            'time_minutes': 45,
+            'price': 15.00
+        }
+        res = self.client.post(RECIPES_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        ingredients = recipe.ingredients.all()
+        self.assertEqual(ingredients.count(), 2)
+        self.assertIn(ingredient1, ingredients)
+        self.assertIn(ingredient2, ingredients)
--- recipe/views.py
+++ recipe/views.py
@@ -34,3 +34,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        return self.serializer_class
+
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
